1296df12fc9449546be9e2bb7f908258c0e59e4a
Dmitriy Babiy <dimascelecto@gmail.com>
2022-10-25 05:40:07 +0700

    Начинаем программировать deleted:    project/src/components/app/app.test.tsx modified:   project/src/components/app/app.tsx new file:   project/src/components/card/card.tsx modified:   project/src/index.tsx new file:   project/src/pages/login-page/login-page.tsx new file:   project/src/pages/main-page/main-page.tsx new file:   project/src/pages/room-page/room-page.tsx

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+export type MainPageProps = {
+  placesCount: number;
+}
+
+const mainPageProps: MainPageProps = {
+  placesCount: 5
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App {...mainPageProps} />
   </React.StrictMode>,
 );
